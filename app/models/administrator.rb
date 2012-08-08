d285cee77aae6b446f21604c614210347805e453
swapna <swapna@swapna.(none)>
2012-08-08 15:42:32 +0530

    Registration pages Modified

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -7,4 +7,12 @@ class Administrator < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :role, :first_name, :last_name, :mobile_number, :status, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  #validations
+
+  validates :first_name , :presence => true
+  validates :last_name , :presence => true
+  validates :mobile_number , :presence => true
+
+
 end
