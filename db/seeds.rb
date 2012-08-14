c9bee312a3625fde37783fc208c232a52d8e4b2e
swapna <swapna@swapna.(none)>
2012-08-14 15:38:58 +0530

    seeds modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#==========================User Looking For Properties ===============================
+
+UserLookingForProperty.create(user_id: 1, location: 'Street Number 3, Krishna Nagar, Yousufguda, Hyderabad, Andhra Pradesh, India', :property_id=> 1, :looking_for_id => 1)
+
+UserLookingForProperty.create(user_id: 2, location: 'Road Number 10, Nandi Nagar, Banjara Hills, Hyderabad, Andhra Pradesh, India', :property_id=> 2, :looking_for_id => 2)
+
+UserLookingForProperty.create(user_id: 1, location: 'Hitech City Rd, Kavuri Hills, Madhapur, Hyderabad, Andhra Pradesh, India', :property_id=> 3, :looking_for_id => 3)
